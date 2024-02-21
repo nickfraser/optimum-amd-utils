74ecc071b3f7518545eeb70ac0061ff82b35fc30
Nick Fraser <icanlosh@gmail.com>
2024-02-21 14:07:33 +0000

    [test] Added tests for "recommended" settings

--- test/brevitas/test_main.py
+++ test/brevitas/test_main.py
@@ -33,6 +33,24 @@ def default_run_args(default_args):
     return args
 
 
+@pytest.fixture()
+def recommended_args(default_args):
+    args = default_args
+    args.apply_gptq = True
+    args.activations_equalization = "layerwise"
+    return args
+
+
+@pytest.fixture()
+def recommended_run_args(default_args):
+    args = default_args
+    args.apply_gptq = True
+    args.activations_equalization = "layerwise"
+    args.nsamples = 2
+    args.seqlen = 2
+    return args
+
+
 @pytest.fixture(params=[False, True])
 def apply_gptq(request):
     yield request.param
@@ -78,7 +96,7 @@ def run_all_args(default_run_args, apply_gptq, apply_weight_equalization, apply_
         [False, False, False, "layerwise", False],
         [False, False, False, None, True],
     ])
-def run_toggle_args(default_run_args, request):
+def toggle_run_args(default_run_args, request):
     args = default_run_args
     args.apply_gptq = request.param[0]
     args.apply_weight_equalization = request.param[1]
@@ -100,8 +118,21 @@ def run_main():
 @pytest.mark.cpu
 @pytest.mark.opt
 @pytest.mark.short
-def test_toggle_opt_cpu(run_main, run_toggle_args):
-    args = run_toggle_args
+def test_toggle_opt_cpu(run_main, toggle_run_args):
+    args = toggle_run_args
+    args.device = "cpu"
+    return_val = run_main(args)
+    assert type(return_val["float_perplexity"]) == torch.Tensor
+    assert type(return_val["quant_perplexity"]) == torch.Tensor
+
+
+@pytest.mark.run
+@pytest.mark.cpu
+@pytest.mark.opt
+@pytest.mark.short
+@pytest.mark.recommended
+def test_recommended_opt_cpu(run_main, recommended_run_args):
+    args = recommended_run_args
     args.device = "cpu"
     return_val = run_main(args)
     assert type(return_val["float_perplexity"]) == torch.Tensor
@@ -124,8 +155,21 @@ def test_all_opt_cpu(run_main, run_all_args):
 @pytest.mark.gpu
 @pytest.mark.opt
 @pytest.mark.short
-def test_toggle_opt_gpu(run_main, run_toggle_args):
-    args = run_toggle_args
+def test_toggle_opt_gpu(run_main, toggle_run_args):
+    args = toggle_run_args
+    args.device = "cuda:0"
+    return_val = run_main(args)
+    assert type(return_val["float_perplexity"]) == torch.Tensor
+    assert type(return_val["quant_perplexity"]) == torch.Tensor
+
+
+@pytest.mark.run
+@pytest.mark.gpu
+@pytest.mark.opt
+@pytest.mark.short
+@pytest.mark.recommended
+def test_recommended_opt_gpu(run_main, recommended_run_args):
+    args = recommended_run_args
     args.device = "cuda:0"
     return_val = run_main(args)
     assert type(return_val["float_perplexity"]) == torch.Tensor
@@ -148,8 +192,21 @@ def test_all_opt_gpu(run_main, run_all_args):
 @pytest.mark.acc
 @pytest.mark.opt
 @pytest.mark.short
-def test_toggle_opt_acc(run_main, run_toggle_args):
-    args = run_toggle_args
+def test_toggle_opt_acc(run_main, toggle_run_args):
+    args = toggle_run_args
+    args.device = "auto"
+    return_val = run_main(args)
+    assert type(return_val["float_perplexity"]) == torch.Tensor
+    assert type(return_val["quant_perplexity"]) == torch.Tensor
+
+
+@pytest.mark.run
+@pytest.mark.acc
+@pytest.mark.opt
+@pytest.mark.short
+@pytest.mark.recommended
+def test_recommended_opt_acc(run_main, recommended_run_args):
+    args = recommended_run_args
     args.device = "auto"
     return_val = run_main(args)
     assert type(return_val["float_perplexity"]) == torch.Tensor
@@ -172,8 +229,22 @@ def test_all_opt_acc(run_main, run_all_args):
 @pytest.mark.acc_offload
 @pytest.mark.opt
 @pytest.mark.short
-def test_toggle_opt_acc_offload(run_main, run_toggle_args):
-    args = run_toggle_args
+def test_toggle_opt_acc_offload(run_main, toggle_run_args):
+    args = toggle_run_args
+    args.device = "auto"
+    args.gpu_device_map = {0: 1.2e8}
+    return_val = run_main(args)
+    assert type(return_val["float_perplexity"]) == torch.Tensor
+    assert type(return_val["quant_perplexity"]) == torch.Tensor
+
+
+@pytest.mark.run
+@pytest.mark.acc_offload
+@pytest.mark.opt
+@pytest.mark.short
+@pytest.mark.recommended
+def test_recommended_opt_acc_offload(run_main, recommended_run_args):
+    args = recommended_run_args
     args.device = "auto"
     args.gpu_device_map = {0: 1.2e8}
     return_val = run_main(args)
