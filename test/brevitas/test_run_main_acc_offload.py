76454263a06bf560d67642502b7ee6169021d09a
Nick Fraser <icanlosh@gmail.com>
2024-02-27 15:11:09 +0000

    [test] Bugfix in setting device map for single-GPU with CPU-offload

--- test/brevitas/test_run_main_acc_offload.py
+++ test/brevitas/test_run_main_acc_offload.py
@@ -22,7 +22,7 @@ def recommended_run_args_acc_offload(recommended_run_args):
 @pytest.fixture()
 def recommended_large_model_run_args_acc_offload(recommended_run_args_acc_offload):
     args = recommended_run_args_acc_offload
-    args.gpu_device_map = {0: calc_gpu_device_map(absolute_mem_margin=2.0 * 1e9, relative_mem_margin=0.3)}
+    args.gpu_device_map = {0: calc_gpu_device_map(absolute_mem_margin=2.0 * 1e9, relative_mem_margin=0.3)[0]}
     return args
 
 
